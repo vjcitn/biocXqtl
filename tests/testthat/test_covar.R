a766c771ff533a3c5a4f77e5b0714a72bafa560a
vjcitn <stvjc@channing.harvard.edu>
2025-12-25 16:55:00 -0500

    covariate testing enhanced

--- tests/testthat/test_covar.R
+++ tests/testthat/test_covar.R
@@ -1,5 +1,3 @@
-
-test_that("getzs with one covariate works", {
 # build RSE with nonCallVars in metadata
    if (!exists("geuv19")) data(geuv19)
    data(geuv19_samples)
@@ -17,8 +15,10 @@ test_that("getzs with one covariate works", {
    covar = as.data.frame(cd[,metadata(geuv19)$nonCallVars])
    names(covar) = metadata(geuv19)$nonCallVars
    mol = assay(geuv19)
-# use lm for a simple model
    litdf = data.frame(y=as.numeric(mol[1,]), snp=calls[,1], Sex=covar[,1])
+
+test_that("getzs with one covariate works", {
+# use lm for a simple model
    m1 = summary(lm(y~snp+Sex, data=litdf))
    b = m1$coefficients[2,1]
    se = m1$coefficients[2,2]
@@ -26,5 +26,20 @@ test_that("getzs with one covariate works", {
    full = getzs(mol[1:10,], calls, covar)
    expect_equal(b/se, full[1,1])
 })
+
+
+test_that("negative binomial fits work with covariate", { 
+  nbz = function(x,y) {
+  # error messages will be thrown from glm ... maybe condition to allow warning/error
+    f = tryCatch(MASS::glm.nb(y~x, data=data.frame(y=y, x=x[,-1])), # switch to -1 here to allow covariates
+             error=function(e) return(list(coefficients=NA, se=NA) ))  # getzs adds column of 1s
+    #if (inherits(f, "try-error")) return(list(coefficients=NA, se=NA))
+    dat = summary(f)$coefficients
+    list(coefficients=dat[,1], se=dat[,2])
+  } 
+   full = getzs(mol[1:10,], calls, covar, statfun=nbz) # includes sex adjustment
+   lit = summary(MASS::glm.nb(y~snp+Sex, data=litdf))
+   expect_true(abs(full[1,1]-lit$coefficients[2,3])<1e-12)
+}
 
 
